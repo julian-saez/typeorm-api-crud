b85240a87dbff0f26dccedc6eea6cd1f89c73762
julian-saez <juliansaez.projects@gmail.com>
2022-12-11 03:49:45 -0300

    refactor: add server file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 require('dotenv').config();
-import "reflect-metadata";
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
+import 'reflect-metadata';
+import app from './config/server';
+import { AppDataSource } from './config/database';
 
-const app = express();
-app.use(morgan('dev'));
-app.use(cors({
-    origin: process.env.URL_DEVELOP
-}));
+const main = async () => {
+    try {
+        await AppDataSource.initialize();
+        console.log('Database connected successfully!');
 
-app.listen(process.env.PORT);
-console.log('Server is listening on Port', process.env.PORT);
+        app.listen(process.env.API_PORT);
+        console.log('Server is listening on Port', process.env.API_PORT);
+    } catch ( err ) {
+        console.error( err )
+    }
+}
 
-
+main();
